196db346963b048d05c659e64d01ae6cfb89a1f8
Rizki Dwi Putra <rizki.jtk07@gmail.com>
2021-11-08 19:23:03 +0700

    Initial Commit

---
+++ rZk-OMDb/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rZk-OMDb
+//
+//  Created by イッキ on 08/11/21.
+//  Copyright © 2021 Rizki Dwi Putra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
